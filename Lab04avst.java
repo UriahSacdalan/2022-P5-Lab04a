d701b0ca8de0ea9fec7dfd3016a5d724874351ea
Uriah <uriahnaomi.sacdalan@vtsd.com>
2022-10-03 12:25:06 -0400

    I finished my Lab04.

--- Lab04avst.java
+++ Lab04avst.java
@@ -7,21 +7,35 @@ public class Lab04avst
 {
     public static void main(String[] args)
     {
-        System.out.println("Lab04a, Student Version\n");
+        System.out.println("Lab04a, 100 Point Version\n");
 
         double principal  = 250000;
         double annualRate = 4.85;
         double numYears   = 30;
 
+        double numMonths = numYears*12;
+        double actualRate = annualRate/100;
+        double monthlyRate = actualRate/12;
 
+        double topParenth = Math.pow(1 + monthlyRate, numMonths);
+        double numerator = monthlyRate * topParenth;
+        double denominator = topParenth - 1;
 
+        double monthlyPay = principal*(numerator/denominator);
+        monthlyPay *= 100;
+        monthlyPay = Math.round(monthlyPay);
+        monthlyPay /= 100;
 
+        double totalPay = (monthlyPay*numMonths);
+        double totalInt = totalPay-principal;
 
+        System.out.println("Principal:         $" + principal);
+        System.out.println("Annual Rate:       " + annualRate + "%");
+        System.out.println("Number of Years:   " + numYears);
+        System.out.println("Monthly Payment:   $" + monthlyPay);
+        System.out.println("Total Payments:    $" + totalPay);
+        System.out.println("Total Interest:    $" + totalInt);
 
-
-
-
-        System.out.println();
     }
 }
 
